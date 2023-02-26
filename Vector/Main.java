2bbfcccf197e0f731c77b78ef54acb491ded04de
hoangphucloc <96833454+hoangphucloc1907@users.noreply.github.com>
2023-02-26 17:34:52 +0700

    Add files via upload

---
+++ Vector/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vector;
+
+/**
+ *
+ * @author sang
+ */
+public class Main {
+    public static void main(String[] args) {
+        Vector a = new Vector(1,2,3);
+        Vector b = new Vector(1,2,1);
+        a.add(b);
+        System.out.println(a.getX() + a.getY() + a.getZ() );
+        
+    }
+}
